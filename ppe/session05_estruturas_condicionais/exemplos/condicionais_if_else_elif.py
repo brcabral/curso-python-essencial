c44fc28f1df357c890cbc79b351ca3d781fd3d4d
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-06-28 14:57:32 -0300

    If, else, elif

---
+++ ppe/session05_estruturas_condicionais/exemplos/condicionais_if_else_elif.py
@@ -0,0 +1,15 @@
+"""
+Estuturas condicionais
+if, else, elif
+"""
+
+idade = 68
+
+if idade < 18:
+    print("Menor de idade")
+elif idade == 18:
+    print("Tem 18 anos")
+elif idade > 60:
+    print("Terceira idade")
+else:
+    print("Maior de idade")
