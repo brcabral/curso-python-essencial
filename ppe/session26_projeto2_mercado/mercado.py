fe2ef65180e118696f5d853b7de605be98c45bc1
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-09-03 22:50:50 -0300

    Implementação - Parte 2

--- ppe/session26_projeto2_mercado/mercado.py
+++ ppe/session26_projeto2_mercado/mercado.py
@@ -78,11 +78,68 @@ def listar_produtos() -> None:
 
 
 def comprar_produto() -> None:
-    pass
+    if len(produtos) > 0:
+        print('Informe o código do produto que deseja adicionar ao carrinho: ')
+        print('==============================================================')
+        print('==================== Produtos disponíveis ====================')
+        for produto in produtos:
+            print(produto)
+            print('--------------------------------------------------------------')
+            sleep(1)
+
+        codigo: int = int(input())
+        produto: Produto = pegar_produto_por_codigo(codigo)
+
+        if produto:
+            if len(carrinho) > 0:
+                tem_no_carrinho: bool = False
+                for item in carrinho:
+                    qtd: int = item.get(produto)
+                    if qtd:
+                        item[produto] = qtd + 1
+                        print(f'O produto {produto.nome} agora possui {item[produto]} unidades no carrinho')
+                        tem_no_carrinho = True
+                        sleep(2)
+                        menu()
+
+                if not tem_no_carrinho:
+                    prod = {produto: 1}
+                    carrinho.append(prod)
+                    print(f'O produto {produto.nome} foi adicionado ao carrinho.')
+                    sleep(2)
+                    menu()
+            else:
+                item = {produto: 1}
+                carrinho.append(item)
+                print(f'O produto {produto.nome} foi adicionado ao carrinho.')
+                sleep(2)
+                menu()
+        else:
+            print(f'O produto com código {codigo} não foi encontrado.')
+            sleep(2)
+            menu()
+    else:
+        print('Ainda não existem produtos para vender.')
+
+    sleep(2)
+    menu()
 
 
 def visualizar_carrinho() -> None:
-    pass
+    if len(carrinho) > 0:
+        print('Produtos no carrinho: ')
+
+        for item in carrinho:
+            for dados in item.items():
+                print(dados[0])
+                print(f'Quantidade: {dados[1]}')
+                print('-----------------------')
+                sleep(1)
+    else:
+        print('Ainda não existem produtos no carrinho.')
+
+    sleep(2)
+    menu()
 
 
 def fechar_pedido() -> None:
@@ -94,7 +151,8 @@ def fechar_pedido() -> None:
             for dados in item.items():
                 print(dados[0])
                 print(f'Quantidade: {dados[1]}')
-                print('=======================')
+                valor_total += dados[0].preco * dados[1]
+                print('-----------------------')
                 sleep(1)
 
         print(f'Sua fatura é {formata_float_str_moeda(valor_total)}')
